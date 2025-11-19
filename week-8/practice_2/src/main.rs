1c5460eede4a5216e980f11c4acee441b876c12b
DJIT <tolu.ddasilva@gmail.com>
2025-11-19 16:16:33 +0100

    (using_get()_method)

--- week-8/practice_2/src/main.rs
+++ week-8/practice_2/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let v = vec!["C","O", "M", "P", "U", "T", "E", "R"];
+    let v = vec!['C','O', 'M', 'P', 'U', 'T', 'E', 'R'];
     let mut input1 = String::new();
 
     println!("Enter an index value btw (0-7)");
