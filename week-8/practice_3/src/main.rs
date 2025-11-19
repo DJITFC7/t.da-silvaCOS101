185c4d5b2b425b34942a1846f334fb1e41c95a49
DJIT <tolu.ddasilva@gmail.com>
2025-11-19 16:31:46 +0100

    (iterating_on_a_vector)

--- week-8/practice_3/src/main.rs
+++ week-8/practice_3/src/main.rs
@@ -4,14 +4,14 @@ fn value (n:Option<&char>)
 }
 
 fn main() {
-    let v = Vec!['R','U','S','T','A','C','I','A','N']
+    let v = vec!['R','U','S','T','A','C','I','A','N'];
 
 let mut input1 = String::new();
 println!("\nEnter an index value btw (0-8)");
-std::io::stdin().read.line(&mut input1).expect("Failed to read input");
+std::io::stdin().read_line(&mut input1).expect("Failed to read input");
 
 //index is the non negative value which is smaller than the size of the vector
-let index:usize = input1.trim().parse().expect("Invalid input")
+let index:usize = input1.trim().parse().expect("Invalid input");
 
 //getting value at given index value
 let ch: Option<&char> = v.get(index);
