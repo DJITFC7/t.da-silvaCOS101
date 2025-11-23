cc81ae30075ab76ba9214eaac3e3139f706f37b4
DJIT <tolu.ddasilva@gmail.com>
2025-11-23 17:12:16 +0100

    (program_using_vectors)

--- CA1/project_3/src/main.rs
+++ CA1/project_3/src/main.rs
@@ -3,31 +3,39 @@ use std::io;
 fn main() {
     let mut input1 = String::new();
     let mut input2 = String::new();
-    let mut input3 = String::new();
- 
+
+  
    println!("Cafe menu\n");
-   println!("T = 800\n");
-   println!("C = 1200\n");
-   println!("S = 2000\n");
-   println!("J = 1500\n");
+   println!("T = £800\n");
+   println!("C = £1200\n");
+   println!("S = £2000\n");
+   println!("J = £1500\n");
  
    println!("Enter in item code");
     io::stdin().read_line(&mut input1).expect("entered a wrong input");
-
+   let code_input:String = input1.trim().parse().expect("not a valid input");
 
     println!("Enter in quantity");
     io::stdin().read_line(&mut input2).expect("entered a wrong input");
-    let q:f32 = input2.trim().parse().expect("not a valid number");
-
-    println!("Do u wish to add more? (y) or (n)");
-    if input3 == "y"{
-        continue;
-    }else if input3 == "n"{
-        break;
-    }else {
-        println!("please enter 'y' or 'n' to continue or stop");
-    }
-
-
+    let q:u16 = input2.trim().parse().expect("not a valid number");
     
-}
+    let te = q * 800;
+    let co = q * 1_200;
+    let sp = q * 2_000;
+    let ju = q * 1_500;
+  // let input1 = input1.to_uppercase();
+  if code_input== "T" {
+    println!("You have picked Tea (£800)");
+    println!("cost is: {}",te);
+  }  else if code_input == "C"{
+    println!("You have picked Coffee (£1200)");
+    println!("cost is: {}",co);
+  }else if code_input == "S" {
+    println!("You have picked Sprite (£2000)");
+    println!("cost is: {}",sp);
+  }else if code_input == "J"{
+    println!("You have picked Juice");
+    println!("cost is: {}",ju);
+  }
+
+ }
