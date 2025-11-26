8d2f6cde48815cfa66c0c21dbfc555a388c50443
DJIT <tolu.ddasilva@gmail.com>
2025-11-26 16:50:16 +0100

    deleting a file with rust

--- week-9/project_1/src/main.rs
+++ week-9/project_1/src/main.rs
@@ -9,6 +9,6 @@ fn main() {
     let mut file = std::fs::File::create("High quality drinks")
     .expect("create failed");
     file.write_all("\nLarger: 33_Export, Desperados, Goldberg, Gulder, Heineken, Star").as_bytes()).expect("write failed");
-    file.write_all("\nstou: Legend, Turbo king, Williams."); 
+    file.write_all("\nstou: Legend, Turbo king., Williams."); 
     println!("Hello, world!");
 }
