6b735fd13086a791b9c08fa6a383972580df4c67
DJIT <tolu.ddasilva@gmail.com>
2025-11-26 16:41:03 +0100

    (deleting_a_file_with_rust)

--- week-9/practice_4/src/main.rs
+++ week-9/practice_4/src/main.rs
@@ -1,4 +1,4 @@
-use std::fs::OpenOptions;
+use::std::fs::OpenOptions;
 use::std::io::Write;
 
 fn main() {
@@ -6,5 +6,6 @@ fn main() {
     let mut file = OpenOptions::new().append(true).open("../practice_1/data.txt").expect("file can't be opened");
     file.write_all("\nHello Class".as_bytes()).expect("Write failed");
     file.write_all("\nThis is the appendage to the document.".as_bytes()).expect("write failed");
+    file.write_all("\nMr Chudi should give me 20 marks and 10 dollars".as_bytes()).expect("write failed");
     println!("filed append success");
 }
---
+++ week-9/project_1/src/main.rs
@@ -0,0 +1,13 @@
+use::std::Write;
+use::std::Read;
+
+
+fn main() {
+    let larger:[&str] = ["33_Export","Desperados","Goldberg","Gulder","Heineken","Star" ];
+    let stout:[&str] = ["Legend", "Turbo King", "Williams"];
+    let non_alchohlic:[&str] = ["Maltina", "Amstel malta","Malta Gold", "Fayrouz"];
+    let mut file = std::fs::File::create("High quality drinks")
+    .expect("create failed");
+    file.write_all("\nLarger: 33_Export, Desperados, Goldberg, Gulder, Heineken, Star",)
+    println!("Hello, world!");
+}
