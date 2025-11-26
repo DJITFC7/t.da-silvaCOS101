2e8ff717ded1e1d596d1ee1126761d67e4c611c4
DJIT <tolu.ddasilva@gmail.com>
2025-11-26 16:48:00 +0100

    creating a file using rust

--- week-9/practice_1/src/main.rs
+++ week-9/practice_1/src/main.rs
@@ -6,7 +6,7 @@ fn main() {
 
     let mut file = std::fs::File::create("data.txt").expect("create failed");
     file.write("Welcome to Rust Programming\n"
-        .as_bytes()).expect("write failed");
+        .as_bytes()).expect("write failed.");
     file.write_all(announce.as_bytes()).expect("write failed");
     file.write_all(dept.as_bytes()).expect("write failed.");
     println!("\nData written to file.");
