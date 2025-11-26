c4549298354f71cf692447fc2690baa454337ead
DJIT <tolu.ddasilva@gmail.com>
2025-11-26 16:46:48 +0100

    adding_a_file_with_rust

--- week-9/project_1/src/main.rs
+++ week-9/project_1/src/main.rs
@@ -8,6 +8,7 @@ fn main() {
     let non_alchohlic:[&str] = ["Maltina", "Amstel malta","Malta Gold", "Fayrouz"];
     let mut file = std::fs::File::create("High quality drinks")
     .expect("create failed");
-    file.write_all("\nLarger: 33_Export, Desperados, Goldberg, Gulder, Heineken, Star",)
+    file.write_all("\nLarger: 33_Export, Desperados, Goldberg, Gulder, Heineken, Star").as_bytes()).expect("write failed");
+    file.write_all("\nstou: Legend, Turbo king, Williams."); 
     println!("Hello, world!");
 }
