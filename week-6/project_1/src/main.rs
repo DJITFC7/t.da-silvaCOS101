8cbde71d7d257952a7b17dd84296b498c2e1fd50
DJIT <tolu.ddasilva@gmail.com>
2026-01-18 17:05:46 +0100

    use of rust and postgreSQL to form database queries

--- week-6/project_1/src/main.rs
+++ week-6/project_1/src/main.rs
@@ -11,31 +11,44 @@ fn main() {
     println!("W = White rice & stew = 2500");
 
 
-    let food_input = loop {
-    let mut input1 = String::new();
-    println!("Please enter food code");
-    io::stdin().read_line(&mut input1).expect("not a valid input");
-    let food_input = input1.trim().to_uppercase();   
-    match food_input.trim().parse::<char>(){
-        Ok(char) => break char,
-        Err(_) => println!("please enter in the food CODE"),
-    }
-};
+//     let food_input = loop {
+//     let mut input1 = String::new();
+//     println!("Please enter food code");
+//     io::stdin().read_line(&mut input1).expect("not a valid input");
+//     let input1 = input1.trim().to_uppercase();   
+//     match input1.trim().parse::<char>(){
+//         Ok(char) => break char,
+//         Err(_) => println!("please enter in the food CODE"),
+//     }
+// };
 
 
-    if food_input >= 'P' {
+
+  let food_input = loop {
+        let mut input = String::new();
+        println!("Please enter food code:");
+        io::stdin().read_line(&mut input).expect("Invalid input");
+
+        match input.trim().to_uppercase().chars().next() {
+            Some('P' | 'F' | 'A' | 'E' | 'W') => break input.chars().next().unwrap(),
+            _ => {println!("Please enter a valid food code");
+            continue; }
+        }
+    };
+
+    if food_input == 'P' {
         println!("order for Poundo yam and Edikaiko soup = 3200 received");
     }
-    else if food_input >= 'f'{
+    else if food_input == 'f'{
         println!("order for Fried rice & chicken = 3000 received");
     }
-    else if food_input >= 'A'  {
+    else if food_input == 'A'  {
         println!("order for Amala and Ewedu soup = 2000 received");
     }
-    else if food_input >= 'E' {
+    else if food_input == 'E' {
         println!("order for Eba and Egusi soup = 2000 received");
     }
-    else if food_input >= 'W' {
+    else if food_input == 'W' {
          println!("order for White rice & stew = 2500 received");
     }
     else {
